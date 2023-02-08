749f4df0dd9e602bc3bf453e167adaf0e5edf4ec
Gayathri-sathish <124767170+Gayathri-sathish@users.noreply.github.com>
2023-02-08 12:53:56 +0530

    Add files via upload
    C Basics Uploaded

---
+++ C/Basics/example3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{	
+	int x=10,y=x,z=x,t;
+	
+	y-=x;
+	z=-x;
+	t=-x;
+	
+	printf("y=%d,z=%d,t=%d",y,z,t);
+
+}
+
---
+++ C/Basics/exmple.c
@@ -0,0 +1,10 @@
+// c example
+#include<stdio.h>
+void main()
+{	
+int a=100,b=200,c;
+c=(a==100||b>200);
+printf("c=%d",c);
+
+}
+
